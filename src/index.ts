12f358317965eb7a8016fb8059fa13a1cf2ad038
prPedr <prpedro025@gmail.com>
2025-11-24 04:43:52 -0300

    Revisao sobre tipagem basica

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-const paciente: {
-    nome: string,
-    idade: string
-} = {
-    nome: "Pedro",
-    idade: "20"
+function desejarBoasVindas(nome: string, idade: string, telefoneFixo?: string) {
+    if (telefoneFixo) {
+        console.log(`Bem vindo ${nome}, voce tem ${idade} anos de idade e seu telefone fixo e ${telefoneFixo}`)
+    } else {
+        console.log(`Bem vindo ${nome}, voce tem ${idade} anos de idade`)
+    }
 }
 
-console.log(paciente)
+const mensagemBoasVindas = desejarBoasVindas("Pedro", "20", "324002-8922")
+
+console.log(mensagemBoasVindas)
