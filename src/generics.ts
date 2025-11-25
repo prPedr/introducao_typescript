9d88210bbf7b58d0a46f6b10e5647791f97ee5bb
prPedr <prpedro025@gmail.com>
2025-11-25 05:26:10 -0300

    Revisando sobre generics

--- src/generics.ts
+++ src/generics.ts
@@ -6,12 +6,20 @@ class Pessoa {
     }
 }
 
-function obterIdentidade<T extends Pessoa>(pessoa: T) {
+function mostrarIdentidade<T extends Pessoa>(pessoa: T) {
     console.log(`Identidade: ${pessoa.identidade}`)
 }
 
 class Medico extends Pessoa {}
+class Enfermeiro extends Pessoa {}
 
-const medicoUm = new Medico("1288378")
+const medicoUm = new Medico("1818829329")
+const medicoDois = new Medico("1980192391")
 
-obterIdentidade(medicoUm)
+const enefermeiroUm = new Enfermeiro("198918389")
+const enfermeiroDois = new Enfermeiro("1901903903")
+
+mostrarIdentidade(medicoUm)
+mostrarIdentidade(medicoDois)
+mostrarIdentidade(enefermeiroUm)
+mostrarIdentidade(enfermeiroDois)
