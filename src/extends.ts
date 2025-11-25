81f440e6733429a94e8a1b7d4741cda5929b80ac
prPedr <prpedro025@gmail.com>
2025-11-25 03:46:42 -0300

    Revisao sobre Extends

--- src/extends.ts
+++ src/extends.ts
@@ -2,33 +2,21 @@ class Pessoa {
     nome: string
     id: string
 
-    constructor(nome: string, id: string) {
+    constructor (nome: string, id: string) {
         this.nome = nome
         this.id = id
     }
 
     apresentar(): void {
-        console.log(`Eu sou o ${this.nome} e meu ID e ${this.id}`)
+        console.log(`${this.nome} ${this.id}`)
     }
 }
 
 class Medico extends Pessoa {
     apresentar(): void {
-        console.log(`Ola, eu sou o ${this.nome} e sou medico e meu ID e ${this.id}`)
+        console.log(`Ola, sou o ${this.nome}, sou Programador e meu ID e ${this.id}`)
     }
 }
 
-class Enfermeiro extends Pessoa {
-    apresentar(): void {
-        console.log(`Ola,eu sou o ${this.nome} e sou medico e meu ID e ${this.id}`)
-    }
-}
-
-const pessoaUm = new Pessoa("Pedro", "1")
-pessoaUm.apresentar()
-
-const medicoUm = new Medico("Gabriel", "2")
-medicoUm.apresentar()
-
-const enfermeiroUm = new Enfermeiro("Carlos", "3")
-enfermeiroUm.apresentar()
+const medico = new Medico("Pedro", "1")
+medico.apresentar()
